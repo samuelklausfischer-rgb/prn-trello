da02f1e6805da9c493e9a459c0483faebc27c24d
Skip <skip@usecurling.com>
2026-04-07 18:33:35 +0000

    Fix: Import useRef in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { useLocation, Link, useNavigate } from 'react-router-dom'
 import {
   Trophy,
